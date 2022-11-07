bbcb1ca4432bae9a1ee69f884d2bf34235f73880
BabyBurrito <mywketer00@mail.ru>
2022-11-07 10:58:37 +0300

    init

---
+++ src/main/java/com/nesterov/tasksexecutortgbot/TasksExecutorTgbotApplication.java
@@ -0,0 +1,13 @@
+package com.nesterov.tasksexecutortgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksExecutorTgbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksExecutorTgbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nesterov/tasksexecutortgbot/TasksExecutorTgbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesterov.tasksexecutortgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksExecutorTgbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
